74eeb38e5f8d79a212aedb79e444433d79c047f2
mukuchi <kosemjared@gmail.com>
2026-02-18 02:41:52 +1100

    ffmpeg

--- bot.py
+++ bot.py
@@ -6,7 +6,10 @@ load_dotenv()
 TOKEN = os.getenv('TOKEN')
 TARGET_USER_ID_STR = os.getenv('TARGET_ID')
 TARGET_USER_ID = int(TARGET_USER_ID_STR)
-AUDIO_FILE = "audio.mp3"
+AUDIO_FILE = discord.FFmpegPCMAudio(
+    "./audio.mp3",
+    executable="/usr/bin/ffmpeg"
+)
 DELAY_BEFORE_PLAY = 3
 LOOP_INTERVAL = 300  # 5 mins
 print(f"Loaded TARGET_USER_ID: {TARGET_USER_ID}")
